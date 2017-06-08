bb86bea91692675855eea8f28c0a1c8f180c453b
Ray.Yan <ray.yan@RayYan-mbp.local>
2017-06-08 18:54:34 +0800

    css header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', children: [] },
   {
-    path: '',
-    children: []
-  }
+    path: 'home',
+    component: HomeComponent,
+    children: [
+      // { path: '', component: Home1Component, pathMatch: 'full' },
+      // { path: 'home1', component: Home1Component },
+      // { path: 'home2', component: Home2Component },
+      // { path: ':id/:name', component: HomeDetailComponent }
+    ]
+  },
 ];
 
 @NgModule({
